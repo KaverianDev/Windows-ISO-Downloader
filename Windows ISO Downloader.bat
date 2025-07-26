9cb80a6c5c534c4491213eccf96c5ffe39357b1b
Kaverian <abaanchou328@gmail.com>
2025-07-26 16:13:43 +0100

    2.02
    added curl checking

--- Windows ISO Downloader.bat
+++ Windows ISO Downloader.bat
@@ -1,47 +1,56 @@
 @echo off
 :: Config
+	:: Check if cURL is available.
+	where curl >nul 2>nul || (
+    echo Please install cURL from https://curl.se/windows/
+	echo. 
+	echo Press any key to exit...
+	pause >nul
+	)
+
+	:: Cosmetics 
 	color 1f
-	set "ver=2.01"
+	set "ver=2.02"
 	echo.
 
-:: Title
-:start
-cls
-echo.
-echo =================================
-echo   Windows ISO Downloader %ver%
-echo =================================
-echo.
-echo   Choose a version:
-echo   -----------------
-echo.
-echo   [1] Windows XP
-echo   [2] Windows Vista
-echo   [3] Windows 7
-echo   [4] Windows 8
-echo   [5] Windows 10
-echo   [6] Windows 11
-echo.
-echo   [0] Exit
-echo.
-echo =================================
-set /p ans=   Enter your version:
+	:: Title
+	:start
+	cls
+	echo.
+	echo =================================
+	echo   Windows ISO Downloader %ver%
+	echo =================================
+	echo.
+	echo   Choose a version:
+	echo   -----------------
+	echo.
+		echo   [1] Windows XP
+		echo   [2] Windows Vista
+		echo   [3] Windows 7
+		echo   [4] Windows 8
+		echo   [5] Windows 10
+		echo   [6] Windows 11
+		echo.
+		echo   [0] Exit
+	echo.
+	echo =================================
+	set /p ans=   Enter your version:
 
-		if "%ans%"=="0" goto :0
-		if "%ans%"=="1" goto :1
-		if "%ans%"=="2" goto :2
-		if "%ans%"=="3" goto :3
-		if "%ans%"=="4" goto :4
-		if "%ans%"=="5" goto :5
-		if "%ans%"=="6" goto :6
+			if "%ans%"=="0" goto :0
+			if "%ans%"=="1" goto :1
+			if "%ans%"=="2" goto :2
+			if "%ans%"=="3" goto :3
+			if "%ans%"=="4" goto :4
+			if "%ans%"=="5" goto :5
+			if "%ans%"=="6" goto :6
 
-	echo Invalid character.
-	pause
+		echo Invalid character.
+		pause
+		goto :start
+
+	:0
+	cls
+		echo Are you sure you want to exit? (Y/N):
+	set /p confirm=
+		if /i "%confirm%"=="Y" exit
 	goto :start
-
-:0
-cls
-echo Are you sure you want to exit? (Y/N):
-set /p confirm=
-if /i "%confirm%"=="Y" exit
-goto :start
